30c225e1f0a8a28c92f77210a5ea33cd5bc315f6
abhi-8004 <abhinavsingh7681@gmail.com>
2026-06-14 22:47:10 +0530

    Minimize Max Distance to Gas Station

---
+++ gas-station-b.s.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int numberOfGasStationsRequired(long double dist, vector<int> &stations)
+{
+    int count = 0;
+    for (int i = 1; i < stations.size(); i++)
+    {
+        long double gap =
+            stations[i] - stations[i - 1];
+
+        int numberInBetween = gap / dist;
+
+        if ((gap / dist) == numberInBetween * dist)
+        {
+            numberInBetween--;
+        }
+
+        count += numberInBetween;
+    }
+
+    return count;
+}
+
+double minimiseMaxDistance(vector<int> &stations, int k)
+{
+
+    long double low = 0;
+    long double high = 0;
+
+    for (int i = 1; i < stations.size(); i++)
+    {
+        high = max(high,
+                   (long double)(stations[i] - stations[i - 1]));
+    }
+
+    long double diff = 1e-6;
+
+    while (high - low > diff)
+    {
+        long double mid =
+            (low + high) / 2.0;
+
+        int required =
+            numberOfGasStationsRequired(mid, stations);
+
+        if (required > k)
+        {
+            low = mid;
+        }
+        else
+        {
+            high = mid;
+        }
+    }
+
+    return (double)high;
+}
+
+int main()
+{
+    vector<int> arr = {2, 3, 5};
+    int k = 4;
+    long double ans = minimiseMaxDistance(arr, k);
+    cout << "The answer is: " << ans << "\n";
+    return 0;
+}
